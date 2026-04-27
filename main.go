7306ab42361199684f972410d7da7cd118c7a414
mirkocuchan <mirko.cuchan01@gmail.com>
2026-04-27 15:09:29 +0000

    repl structure

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "strings"
+import (
+	"bufio"	
+	"os"
+	"strings"
+	"fmt"
+)
 
 func cleanInput(text string) []string{
     output := strings.ToLower(text)
     return strings.Fields(output)
 }
 
+func main(){
+	reader := bufio.NewScanner(os.Stdin)
+	for{
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+		
+		words := cleanInput(reader.Text())
+			
+		if len(words) == 0 {
+			continue 
+		}
+
+		fmt.Println("Your command was:", words[0])
+	}
+}
